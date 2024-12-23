5b8a0e118d4977ffbef00d55b5f51f949da72e1e
yejoo0310 <yejoo0310@naver.com>
2024-12-23 17:31:59 +0900

    ^_^...

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express, { json } from "express";
 
-import testRouter from "#routes/test";
+import testRouter from "./src/routes/test.js";
 
 const app = express();
 const port = 8080;
 
 app.use(json());
 
-app.use("/test", testRouter);
+app.use("/api/test", testRouter);
 
 app.listen(port, () => {
   console.log(`${port} 번 서버 킴`);
