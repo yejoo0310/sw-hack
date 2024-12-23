9fe5aaa3a33bbb2c49c497b3c365b3351b17eb3b
yejoo0310 <yejoo0310@naver.com>
2024-12-23 17:52:48 +0900

    database와 연결

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express, { json } from "express";
 
 import testRouter from "./src/routes/test.js";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 8080;
@@ -8,6 +12,8 @@ const port = 8080;
 app.use(json());
 
 app.use("/api/test", testRouter);
+
+mongoose.connect(process.env.MONGODB_URL).then(() => console.log("db 연결 됨"));
 
 app.listen(port, () => {
   console.log(`${port} 번 서버 킴`);
