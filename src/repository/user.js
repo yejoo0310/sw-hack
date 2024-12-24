4dfc2d5fbba07a0ffd2db800ffece7a17656e51e
yejoo0310 <yejoo0310@naver.com>
2024-12-25 00:04:25 +0900

    로그인 기능 진짜 완성

--- src/repository/user.js
+++ src/repository/user.js
@@ -11,6 +11,12 @@ import UserModel from "../models/user.js";
 // };
 
 const login = async (username, password) => {
+  console.log("username", username);
+  console.log("password", password);
+
+  const a = await UserModel.find();
+  console.log(a);
+
   const user = await UserModel.findOne(
     {
       username, // find 조건
@@ -19,11 +25,14 @@ const login = async (username, password) => {
     { password: 0 } // project 조건
   ).lean();
 
-  if (!user || user.password !== password) {
+  if (!user) {
     return null;
   }
 
-  return user;
+  return {
+    ...user,
+    id: user._id,
+  };
 };
 
 export default { login };
